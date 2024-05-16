afcdeef24dd21e0d221558449d0a97d42dc4e618
sina rasouli <sinarasouli79@gmail.com>
2024-05-16 22:23:13 +0330

    feat:add courses command

--- main.py
+++ main.py
@@ -1,44 +1,59 @@
-import os 
+import os
+
+import httpx
+from dotenv import load_dotenv
 from telegram import ForceReply, Update
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
-from  dotenv import load_dotenv
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+    MessageHandler,
+    filters,
+)
 
 load_dotenv()
-async def start(update:Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """ send a meassage whe the command /start is issued. """
+EDUHUB_URL = os.loadenv("EDUHUB_URL")
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """send a meassage whe the command /start is issued."""
     user = update.effective_user
     await update.message.reply_html(
-        rf"hi {user.mention_html()}!",
-        reply_markup=ForceReply(selective=True)
+        rf"hi {user.mention_html()}!", reply_markup=ForceReply(selective=True)
     )
+
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /help is issued."""
     user = update.effective_user
 
     await update.message.reply_html(
-
         rf"Hi {user.mention_html()}!",
-
         reply_markup=ForceReply(selective=True),
-
     )
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Echo the user message."""
 
     await update.message.reply_text(update.message.text)
 
 
+async def get_courses(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    courses_url = f"{ os.getenv('EDUHUB_URL') }courses/"
+    client = httpx.AsyncClient()
+    response = await client.get(courses_url)
+    courses_data = response.json()
+    await client.aclose()
+    await update.message.reply_text(courses_data)
+
 
 def main() -> None:
-
     """Start the bot."""
 
     # Create the Application and pass it your bot's token.
 
     application = Application.builder().token(os.getenv("EDUHUB_BOT_TOKEN")).build()
-
 
     # on different commands - answer in Telegram
 
@@ -46,16 +61,15 @@ def main() -> None:
 
     application.add_handler(CommandHandler("help", help_command))
 
+    application.add_handler(CommandHandler("courses", get_courses))
 
     # on non command i.e message - echo the message on Telegram
 
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
-
     # Run the bot until the user presses Ctrl-C
 
     application.run_polling(allowed_updates=Update.ALL_TYPES)
-
 
 
 if __name__ == "__main__":
